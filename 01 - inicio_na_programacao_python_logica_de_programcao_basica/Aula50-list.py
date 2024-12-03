2fcc3f1a2da0c22e625bad44ca22d814325e2f80
Paulo Carvalho <paulo.carvalho@global.ntt>
2024-12-03 12:19:06 -0300

    iniciando uma lista

---
+++ 01 - inicio_na_programacao_python_logica_de_programcao_basica/Aula50-list.py
@@ -0,0 +1,24 @@
+"""
+Lista python
+Tipo de listas
+Suporte vários valores de qualquer tipo
+conhecimentos reutilizaveis - indices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+
+"""
+#  12345
+# -54321
+
+string = 'ABCDE' # 5 Caracteres 
+# print(string[-1])
+# print(string[0])
+
+
+#        0    1      2                3      4
+#       -5   -4     -3               -2     -1
+lista = [123, True, 'Paulo Carvalho', 12.55, []] # ''
+lista[-3] = "Andreia" 
+print(lista)
+print(lista[2], type(lista[2]))
+# print(bool(lista)) se alista estiver vazia éla é falsa
+# print(lista, type(lista))
